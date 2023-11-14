767f880dd284c6600f2010dae0c7c72688f4a4a3
ArtemChubur <artemchubur@gmail.com>
2023-11-14 22:51:30 +0600

    add sort

--- src/pages/weather/weather.jsx
+++ src/pages/weather/weather.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
-import Header from '../header/header'
-import Main from '../main/main'
+// import Header from '../header/header'
+// import Main from '../main/main'
+import Header from '../weather/header/header'
+import Main from '../weather/main/main'
 
 const Weather = () => {
   return (
